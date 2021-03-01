4c2f8195bf02d748a1a3e50f823e3adfd1b2d8b0
Ishtiak Ahmed <iahmed1606@gmail.com>
2021-03-01 11:36:14 +0600

    Player image added

--- src/Components/Player/Player.js
+++ src/Components/Player/Player.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Player.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
 const Player = (props) => {
     const { name, role, salary, img } = props.player;
@@ -9,13 +11,13 @@ const Player = (props) => {
     return (
         <div className="player">
             <div className="avatar">
-                <img src={img} className="rounded-circle" alt="" />
+                <img src={img} alt="" />
             </div>
             <div className="info">
                 <p className="fs-6"><strong>{name}</strong></p>
                 <p><small>Role: {role}</small></p>
                 <p><small>Salary: ${salary}</small></p>
-                <button className="btn btn-warning btn-sm" onClick={() => handleAddPlayer(props.player)}>Add To Team</button>
+                <button className="btn btn-warning btn-sm" onClick={() => handleAddPlayer(props.player)}><FontAwesomeIcon icon={faPlus}></FontAwesomeIcon> Add To Team</button>
             </div>
         </div >
     );
